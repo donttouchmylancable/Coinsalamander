7c5b2be6c4331b3c6a2627fcfb7fe25a6cea4c0b
donttouchmylancable <128171267+donttouchmylancable@users.noreply.github.com>
2023-08-08 00:31:24 +0200

    added simple animation on scroll for coincards

--- src/components/coin.jsx
+++ src/components/coin.jsx
@@ -1,7 +1,18 @@
+import {useState,useRef, useEffect} from 'react'
 import selectedCoin from '../App'
 function coin(c){
+    const [open,setOpen]=useState(false);//visible
+    const myRef= useRef();
+    useEffect(()=>{
+        
+        const observer= new IntersectionObserver((entries)=>{
+            const entry=entries[0]
+            setOpen(entry.isIntersecting);
+        })
+        observer.observe(myRef.current)
+    },[])
     return(
-        <div className="coincard" onClick={()=>{c.setCoin(c.info.id.toLowerCase());console.log('coincard clicked')}} >
+        <div ref={myRef} className={open?'coincard':'coincard_invisible'} onClick={()=>{c.setCoin(c.info.id.toLowerCase());console.log('coincard clicked')}} >
             <h1 id={'rank'} className="rank">{c.info.market_cap_rank}.</h1>
             <img id={'coinlogo'} src={c.info.image}></img>
             <div id={'nameandsymbol'}>
